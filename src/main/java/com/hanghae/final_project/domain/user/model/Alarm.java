dcce3796a01c83562182a64d539f3d3bd0310da7
ingwon97 <ghkdrnjs180@naver.com>
2022-08-29 11:00:00 +0900

    feat: domain 추가

---
+++ src/main/java/com/hanghae/final_project/domain/user/model/Alarm.java
@@ -0,0 +1,33 @@
+package com.hanghae.final_project.domain.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Alarm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "workspace_users_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Workspace_Users workspace_users;
+
+    @JoinColumn(name = "workspace_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Workspace workspace;
+
+    @JoinColumn(name = "user_id", nullable = true)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+}
---
+++ src/main/java/com/hanghae/final_project/domain/user/model/Workspace_Users.java
@@ -0,0 +1,29 @@
+package com.hanghae.final_project.domain.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Workspace_Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workspace_id", nullable = false)
+    private Workspace workspace;
+
+}
