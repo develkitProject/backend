8f58955acdaca241cfa6a4b9121b19310a15e97a
hosung <winsomed96@naver.com>
2022-08-29 11:19:34 +0900

    Feat : add project's entities

---
+++ src/main/java/com/hanghae/final_project/domain/workspace/model/Notice.java
@@ -0,0 +1,34 @@
+package com.hanghae.final_project.domain.workspace.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+
+public class Notice extends Timestamped{
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workspace_id")
+    private WorkSpace workSpace;
+
+}
---
+++ src/main/java/com/hanghae/final_project/domain/workspace/model/WorkSpace.java
@@ -0,0 +1,31 @@
+package com.hanghae.final_project.domain.workspace.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+public class WorkSpace extends Timestamped {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String desc;
+
+
+
+
+}
