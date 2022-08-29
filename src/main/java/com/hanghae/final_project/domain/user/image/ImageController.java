479f0e9c2496bc67bf8d7d1cd4a92b298465f772
ingwon97 <ghkdrnjs180@naver.com>
2022-08-29 10:27:11 +0900

    S3 업로딩 기능 추가

---
+++ src/main/java/com/hanghae/final_project/domain/user/image/ImageController.java
@@ -0,0 +1,30 @@
+package com.hanghae.final_project.domain.user.image;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+public class ImageController {
+
+    private final S3UploaderService s3UploaderService;
+
+    @PostMapping("/api/images")
+    public String upload(@RequestParam("images") String images) throws IOException {
+        s3UploaderService.upload(images, "static");
+        return "test";
+    }
+
+    @DeleteMapping("/api/images")
+    public String deleteFile(@RequestParam("filename") String filename) {
+        System.out.println(filename);
+        s3UploaderService.deleteImage(filename);
+        return "delete";
+    }
+
+}
