547d58bfdf0676c8c54553fe159b5bf46b684756
ingwon97 <ghkdrnjs180@naver.com>
2022-08-30 19:15:21 +0900

    Feat : by Redis pub/sub

--- src/main/java/com/hanghae/final_project/domain/websocket/chat/ChatController.java
+++ src/main/java/com/hanghae/final_project/domain/websocket/chat/ChatController.java
@@ -1,25 +1,28 @@
 package com.hanghae.final_project.domain.websocket.chat;
 
+import com.hanghae.final_project.domain.websocket.redis.RedisPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-// import 생략...
 
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
 
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
 
+    /**
+     * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
+     */
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
-        if (ChatMessage.MessageType.JOIN.equals(message.getType()))
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
 }
--- src/main/java/com/hanghae/final_project/domain/websocket/chat/ChatRoom.java
+++ src/main/java/com/hanghae/final_project/domain/websocket/chat/ChatRoom.java
@@ -6,22 +6,19 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
-@Getter @Setter
-@NoArgsConstructor
-public class ChatRoom {
+@Getter
+@Setter
+public class ChatRoom implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006639L;
+
     private String roomId;
     private String name;
-    private Set<WebSocketSession> sessions = new HashSet<>();
-
-    @Builder
-    public ChatRoom(String roomId, String name) {
-        this.roomId = roomId;
-        this.name = name;
-    }
 
     public static ChatRoom create(String name) {
         ChatRoom chatRoom = new ChatRoom();
