157a2a502ca077de9bd9a49f9696ff553b0640ed
hosung <winsomed96@naver.com>
2022-09-06 18:14:22 +0900

    Feat : make Invitation code API complete

---
+++ src/main/java/com/hanghae/final_project/domain/workspace/repository/InvitationRepository.java
@@ -0,0 +1,11 @@
+package com.hanghae.final_project.domain.workspace.repository;
+
+import com.hanghae.final_project.domain.workspace.model.Invitation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InvitationRepository extends JpaRepository<Invitation,Long> {
+
+    Optional<Invitation> findByWorkSpace_Id(Long workspaceId);
+}
