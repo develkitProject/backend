bd36011e3c6d09ae2b9129d7eefd148d5af230f9
cheoljun0422 <ljc3484@naver.com>
2022-09-15 21:24:09 +0900

    Revert "Revert "docs: sort by createdAt #41
    This reverts commit a47e71833788e38a6e38ce0c9afd3f2ff53d540d.

--- src/main/java/com/hanghae/final_project/domain/workspace/repository/DocumentRepository.java
+++ src/main/java/com/hanghae/final_project/domain/workspace/repository/DocumentRepository.java
@@ -1,21 +1,14 @@
 package com.hanghae.final_project.domain.workspace.repository;
 
-import com.hanghae.final_project.domain.user.model.User;
 import com.hanghae.final_project.domain.workspace.model.Document;
-import com.hanghae.final_project.domain.workspace.model.WorkSpace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface DocumentRepository extends JpaRepository<Document, Long> {
 
-    User findByUser(User user);
+    List<Document> findAllByWorkSpaceIdOrderByCreatedAtDesc(Long id);
 
-    Document findByWorkSpaceId(Long workSpaceId);
+    List<Document> findAllByWorkSpaceId(Long id);
 
-    List<Document> findAllByWorkSpaceId(Long workSpaceId);
-
-
-    List<Document> findAllByWorkSpaceIdOrderByCreatedAtDesc(Long workspaceId);
 }
