03478eee6c012634cf970adcb25d4fa8e885087f
hosung <winsomed96@naver.com>
2022-09-26 15:43:22 +0900

    Fix : update getWorkspace API's reponse dto

--- src/main/java/com/hanghae/final_project/domain/workspace/repository/DocumentRepository.java
+++ src/main/java/com/hanghae/final_project/domain/workspace/repository/DocumentRepository.java
@@ -5,11 +5,13 @@ import com.hanghae.final_project.domain.workspace.model.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DocumentRepository extends JpaRepository<Document, Long>{
 
     List<Document> findAllByWorkSpaceIdOrderByCreatedAtDesc(Long id);
 
+    Optional<Document> findFirstByWorkSpaceIdOrderByCreatedAtDesc(Long workspaceId);
     Long countDocumentByUser(User user);
     List<Document> findAllByWorkSpaceId(Long id);
 
